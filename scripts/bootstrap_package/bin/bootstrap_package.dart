041e15c662ba35fceee3907c27084cc012d00b25
masaki <masakisato.japan@gmail.com>
2023-12-06 17:19:17 +0900

    delete unnecessary comments

--- scripts/bootstrap_package/bin/bootstrap_package.dart
+++ scripts/bootstrap_package/bin/bootstrap_package.dart
@@ -2,7 +2,7 @@ import 'package:bootstrap_package/run_command.dart';
 
 /// 新規パッケージ作成用の関数
 ///
-/// FDSプロジェクトのルートディレクトリから以下のコマンドにより実行する。
+/// プロジェクトのルートディレクトリから以下のコマンドにより実行する。
 /// `fvm dart run bootstrap_package <パッケージ名>`
 /// 実際の処理内容は[runCommand]に記述する。
 void main(List<String> args) => runCommand(args);
