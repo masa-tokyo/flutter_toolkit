3c7d61210a3cc1bd955c3c775c65c4ae4ff2c955
masaki <masakisato.japan@gmail.com>
2025-02-18 16:26:38 +0900

    add optional workspace flag

--- scripts/bootstrap_package/lib/overwrite_pubspec_yaml_file.dart
+++ scripts/bootstrap_package/lib/overwrite_pubspec_yaml_file.dart
@@ -17,22 +17,27 @@ import 'run_flutter.dart';
 /// - 不要なコメントの削除
 /// - 意図しない配信を避けるためpublish_toフィールドを追加
 /// - uses-material-designフィールドを追加
+/// - [isWorkspace] に応じてresolutionフィールドを追加
+/// - [dependencies], [devDependencies] を依存関係へ追加
 void overwritePubspecYamlFile({
   required String packageName,
   required String description,
   required List<String> dependencies,
   required List<String> devDependencies,
+  required bool isWorkspace,
 }) {
   final dartVersion = getDartCaretVersion();
+  final resolution = isWorkspace ? '  resolution: workspace\n' : '';
 
   final content = '''
 name: $packageName
 description: $description
 publish_to: 'none'
 version: 0.0.1
-
+$resolution
 environment:
   sdk: $dartVersion
+resolution: workspace
 
 dependencies:
   flutter:
